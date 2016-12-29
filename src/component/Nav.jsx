ae4d3a0c4255ab2e987b52ea51035c37336f7799
faisalsahak <sahak.faisal@gmail.com>
2016-12-28 16:38:55 -0800

    added 2 folders for the scss and used foundation library to add styling and also link the links to the proper components

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,10 +5,21 @@ class Nav extends Component {
   render() {
 
     return(
-      <div>
-        <h1>React Timer App</h1>
-        <IndexLink to= '/timer' >Timer</IndexLink>
-        <IndexLink to= '/countdown'>Countdown</IndexLink>
+      <div className = "top-bar">
+        <div className = "top-bar-left">
+          <ul className= "menu">
+            <li className= 'menu-text'>React Timer App</li>
+            <li><IndexLink to= '/timer' >Timer</IndexLink></li>
+            <li><Link to= '/countdown'>Countdown</Link></li>
+          </ul>
+        </div>
+        <div className = "top-bar-right">
+          <ul className = "menu-text">
+            <li className= 'menu-text'>
+              Created By <a href = "#">Click</a>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
